3378293080ee29ff1b2e22bbdeadf5e32007d3c8
Franz Schnyder <schnyderfranz@gmail.com>
2011-02-24 06:25:45 +0000

    Produkt-irrelevant: NubsLauncher Unit tests.
    git-svn-id: https://svn.nmag.ch/svn/NMS/tools/NubsLauncher/trunk@12530 2681232d-325c-0410-86a2-0b9e1caccf8b

--- source/java/src/com/netstal/tools/nubs/launcher/infrastructure/Stream.java
+++ source/java/src/com/netstal/tools/nubs/launcher/infrastructure/Stream.java
@@ -4,6 +4,9 @@ import java.io.Closeable;
 import java.io.IOException;
 
 public class Stream {
+   
+   private Stream() {}
+   
    public static void close(Closeable closeable) {
       if (closeable!=null) {
          try {
---
+++ source/java/test/com/netstal/tools/nubs/launcher/infrastructure/StreamTest.java
@@ -0,0 +1,43 @@
+package com.netstal.tools.nubs.launcher.infrastructure;
+
+import static org.easymock.EasyMock.createControl;
+import static org.easymock.EasyMock.expectLastCall;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.easymock.IMocksControl;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StreamTest {
+
+   private IMocksControl control;
+   private Closeable closeable;
+
+
+   @Before
+   public void setUp() {
+      control = createControl();
+      closeable = control.createMock("closeable",Closeable.class);      
+   }
+   
+   
+   @Test
+   public void testClose() throws IOException {
+      closeable.close();
+      control.replay();
+      Stream.close(closeable);
+      control.verify();
+   }
+   
+   @Test
+   public void testCloseThrows() throws IOException {
+      closeable.close();
+      expectLastCall().andThrow(new IOException());
+      control.replay();
+      Stream.close(closeable);
+      control.verify();
+   }
+
+}
