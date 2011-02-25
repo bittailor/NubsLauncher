79df5c538fda0613e832d03fd4436f14dd988d3f
Franz Schnyder <schnyderfranz@gmail.com>
2011-02-25 10:17:43 +0000

    Produkt-irrelevant: NUBS Launcher install.rb update.
    git-svn-id: https://svn.nmag.ch/svn/NMS/tools/NubsLauncher/trunk@12561 2681232d-325c-0410-86a2-0b9e1caccf8b

--- source/install/install.rb
+++ source/install/install.rb
@@ -16,10 +16,13 @@ rake_bat = `utility\\which.cmd rake.bat`.strip.gsub("\\","/")
 install_dir = File.dirname(rake_bat).gsub("/","\\")
 
 old_nubs = `utility\\which.cmd nubs.exe`.strip
+puts "old_nubs @ #{old_nubs}" 
 
-if !old_nubs.nil? && !old_nubs.empty? && File.dirname(old_nubs) != install_dir && File.dirname(old_nubs) != server_dir.gsub("/","\\")   
-   puts "delete old nubs from #{old_nubs}" 
-   system("del #{old_nubs}")
+if !old_nubs.nil? && !old_nubs.empty? && File.dirname(old_nubs).downcase != install_dir.downcase && File.dirname(old_nubs).downcase != server_dir.gsub("/","\\").downcase   
+   if !(old_nubs.downcase=="s:\Gruppe\TE\Verwaltung\Tools\NUBSLauncher\nubs.exe".downcase && old_nubs.downcase=="nubs.exe".downcase ) 
+      puts "delete old nubs from #{old_nubs}" 
+      system("del #{old_nubs}")
+   end
 end
 puts "install nubs version #{version} to #{install_dir}"
 FileUtils.cp("nubs.exe",install_dir)
