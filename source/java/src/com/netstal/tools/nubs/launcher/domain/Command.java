9fad96b598eb8f1a1ac09357c721acb956cb494f
Franz Schnyder <schnyderfranz@gmail.com>
2011-03-11 14:11:21 +0000

    Produkt-irrelevant: NubsLauncher Fix internal launcher with multiple tasks
    git-svn-id: https://svn.nmag.ch/svn/NMS/tools/NubsLauncher/trunk@12999 2681232d-325c-0410-86a2-0b9e1caccf8b

--- source/java/src/com/netstal/tools/nubs/launcher/domain/Command.java
+++ source/java/src/com/netstal/tools/nubs/launcher/domain/Command.java
@@ -6,7 +6,12 @@
 
 package com.netstal.tools.nubs.launcher.domain;
 
+import java.util.*;
+import java.util.logging.*;
+
 public class Command {
+   
+   private static Logger LOG = Logger.getLogger(Command.class.getName());
    
    // TODO fsc fix !
    public static final String RAKE = isWindows() ? "rake.bat" : "rake";
@@ -21,9 +26,55 @@ public class Command {
    }
 
    public String[] command() {
-      return new String[]{RAKE,command.trim()}; 
+      List<String> commandParts = new LinkedList<String>();
+      commandParts.add(RAKE);
+
+     
+      StringBuffer buffer = new StringBuffer(); 
+      String[] split = command.trim().split(" ");
+      for (String string : split) {
+         if (buffer.length()==0) {
+            if (evenQuotes(string)) {
+               commandParts.add(doubleQuotes(string));
+            } else {
+               buffer.append(string);
+            }
+         } else {
+            buffer.append(" ");
+            buffer.append(string);
+            if (evenQuotes(buffer.toString())) {
+               commandParts.add(doubleQuotes(buffer.toString()));
+               buffer = new StringBuffer();
+            }
+         }
+         
+      }  
+      
+      String[] commandArray = commandParts.toArray(new String[commandParts.size()]);
+      
+      LOG.log(Level.INFO,"commandArray : ");
+      for (String string : commandArray) {
+         LOG.log(Level.INFO,"  "+string);
+      }
+      
+      
+      return commandArray; 
+   }
+
+   private boolean evenQuotes(String string) {
+      int count = countQuotes(string);
+      return Math.abs(count)%2==0;
    }
    
+   private int countQuotes(String string) {
+      String quotes = string.replaceAll("[^\"]", "");
+      return quotes.length();
+   }
+   
+   private String doubleQuotes(String string) {
+      return string.replaceAll("[\"]", "\"\"");
+   }
+
    public String rakeCmdLine() {
       return RAKE + " " + command; 
    }
